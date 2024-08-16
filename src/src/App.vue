ee1b5f52fdad693a85f42031e54fee3421e2ff25
groomy790 <groomusemail@gmail.com>
2024-08-16 23:09:19 +0900

    FIX: Remove unnecessary import

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import { RouterView, RouterLink } from 'vue-router'
+    import { RouterView } from 'vue-router'
 
     import Nav from './components/nav.vue'
 </script>
