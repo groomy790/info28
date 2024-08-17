4b1ec3604073ad9ba23ca25b2153bb6b5a4d3a36
groomy790 <groomusemail@gmail.com>
2024-08-17 20:30:53 +0900

    REFACTOR: refactorying of expirecheck

--- src/src/utils/expirecheck.ts
+++ src/src/utils/expirecheck.ts
@@ -5,19 +5,13 @@ const today_day = now.getDate()
 
 export const ExpireCheck = (year: number, month: number, day: number) => {
     if (year > today_year) return false
-
-    if (year === today_year) {
-        if (month > today_month) return false
-        
-        if (month === today_month) {
-            if (day >= today_day) return false
-        }
-    }
+    if (month > today_month) return false
+    if (day >= today_day) return false
 
     return true
 }
 
-// const chlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-// chlist.forEach(item => {
-//     console.log(`expire ${today_year} ${today_month} ${item}`, ExpireCheck(today_year, today_month, item))
-// })
+const chlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+chlist.forEach(item => {
+    console.log(`expire ${today_year} ${today_month} ${item}`, ExpireCheck(today_year, today_month, item))
+})
