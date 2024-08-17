9ceb312b492d6c3f1d2c696efb485698682a85e0
groomy790 <groomusemail@gmail.com>
2024-08-17 19:58:57 +0900

    Add firestore for internal

--- src/src/pages/internal.vue
+++ src/src/pages/internal.vue
@@ -1,21 +1,34 @@
 <script setup lang="ts">
+    import { ref, onMounted } from 'vue'
     import { GetInternalCollection } from '../service/internal'
-
+    import { InternalCollection } from '../model/internal'
     import Card from '../components/card.vue'
 
-    const collections = GetInternalCollection()
+    const collections = ref<InternalCollection>([])
+
+    const fetchCollections = async () => {
+        try {
+            collections.value = await GetInternalCollection()
+        } catch (error) {
+            console.error('Error fetching collections:', error)
+        }
+    }
+
+    onMounted(() => {
+        fetchCollections();
+    })
 </script>
 
 <template>
-    <h1>교내 행사</h1>
+  <h1>교내 행사</h1>
 
-    <div class="auto-grid my-2">
-        <Card   v-for="item in collections"
-                :name="item.name"
-                :tags="[item.subject, item.category]"
-                :body="item.body"
-                :links="item.links"
-                :expire="item.expire"
-                :deprecated="false" />
-    </div>
+  <div class="auto-grid my-2">
+    <Card v-for="item in collections" :key="item.name" 
+          :name="item.name"
+          :tags="[item.subject, item.category]"
+          :body="item.body"
+          :links="item.links"
+          :expire="item.expire"
+          :deprecated="false" />
+  </div>
 </template>
--- src/src/service/internal.ts
+++ src/src/service/internal.ts
@@ -1,28 +1,19 @@
-import { InternalCollection } from "../model/internal"
+import { Internal, InternalCollection } from "../model/internal"
 
-const FAKE_ASS_DATA: InternalCollection = [
-    {
-        name: "수능dasdasfasdf",
-        subject: "전asfdasf과목",
-        category: "비sfasdf상",
-        body: ["A", "B", "sdffC"],
-        expire: { year: 2024, month: 12, day: 31},
-        links: [
-            { name: "asasdfdsfdad", url: "https://www.google.com" }
-        ]
-    },
-    {
-        name: "동아리 신&*&(&%*청",
-        subject: "기<><><><><>타",
-        category: "비asdf87b상",
-        body: ["Aasf", "Bsafsada*(H&*(&)(SFDf", "Casdfasdf"],
-        expire: { year: 2024, month: 12, day: 31},
-        links: [
-            { name: "sf2gddad", url: "https://www.google.com" }
-        ]
-    }
-]
+import { firestoreDB } from "../firebase/init"
+import { collection, getDocs } from "firebase/firestore"
 
-export function GetInternalCollection() {
-    return FAKE_ASS_DATA
+const firestore_internal_collection = collection(firestoreDB, "internal")
+
+export async function GetInternalCollection() {
+    const collection: InternalCollection = []
+
+    const snapshot = await getDocs(firestore_internal_collection)
+    snapshot.forEach(item => {
+        collection.push(item.data() as unknown as Internal)
+    })
+    
+    console.log("Internal collection required:", collection)
+    
+    return collection
 }
